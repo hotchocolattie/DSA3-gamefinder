93bb76b78b50c3bfcf8f8dd41a54cbc371062812
Laurence Georges <lrncgeorges@gmail.com>
2025-04-18 03:16:32 -0400

    I'm done with common functions for the redblacktree class, lmk if there's something else we may need.

--- DSA3-gamefinder/src/RedBlackTree.cpp
+++ DSA3-gamefinder/src/RedBlackTree.cpp
@@ -31,25 +31,107 @@ Node* RedBlackTree::helperInsert(Node* curr, string name, int rating) {
         // if the parent and the child are red, and the uncle is red, recolor the parent and uncle to black and check the grandparent
 
     }
+    if (rating == curr->rating) {
+        if (name<curr->name) {
+            curr->left = helperInsert(curr->left, name, rating);
+            curr->left->parent = curr;
+            curr->left->red = true;
+        }
+        else {
+            curr->right = helperInsert(curr->right, name, rating);
+            curr->right->parent = curr;
+            curr->right->red = true;
+        }
+    }
      return curr;
 
 }
 
-void RedBlackTree::case_violation(Node* curr) {
-    Node* parent  = curr->parent;
-    Node* grandparent = parent->parent;
-    if (parent->red == true && curr->red ==true) {
-        parent->red = false;
-        if (curr == parent->left) {
-            grandparent->right->red = false;
-        } else if(curr == parent->right) {
-            grandparent->left->red = false;
-        }
-        grandparent->red = true;
+Node* RedBlackTree::rotate_left(Node* parent) {
+    Node* new_parent = parent->right;
+    Node* temp = new_parent->left;
+    new_parent->left = parent;
+    parent->right = temp;
+    if (temp) {
+        temp->parent = parent;
     }
-    case_violation(grandparent);
+    new_parent->parent = parent->parent;
+    parent->parent = new_parent;
+    return new_parent;
+
 }
 
+Node* RedBlackTree::rotate_right(Node* parent) {
+    Node* new_parent = parent->left;
+    Node* temp = new_parent->right;
+    new_parent->left = parent;
+    parent->left = temp;
+    if (temp) {
+        temp->parent = parent;
+    }
+    new_parent->parent = parent->parent;
+    parent->parent = new_parent;
+    return new_parent;
+
+}
+
+void RedBlackTree::case_violation(Node* curr) {
+    if (!curr || !curr->parent || !curr->parent->parent) {
+        return;
+    }
+    Node* parent = curr->parent;
+    Node* grandparent = parent->parent;
+    if (curr->red == true && parent->red == true) {
+        Node* uncle = (grandparent->left) ? grandparent->left : grandparent->right;
+        if (uncle && uncle->red == true) { // when the uncle is red
+            uncle->red = false;
+            curr->red = false;
+            grandparent->red = true;
+            case_violation(grandparent);
+        }
+        if (uncle && uncle->red == false) { // when the uncle is black
+            if (parent == grandparent->left && curr == parent->right) {// left right case
+                curr = rotate_left(parent);
+                curr->parent->red = false;
+                grandparent->red =true;
+                grandparent = rotate_right(grandparent);
+                rotate_helper(curr->parent,grandparent);
+            }
+            else if (parent == grandparent->right && curr == parent->left) { // right left case
+                curr = rotate_right(parent);
+                curr->parent->red = false;
+                grandparent->red = true;
+                grandparent = rotate_left(grandparent);
+                rotate_helper(curr->parent,grandparent);
+            }
+            else if (parent == grandparent->left && curr == parent->left) {// left left case
+                 parent->red = false;
+                 grandparent->red = true;
+                 grandparent = rotate_right(grandparent);
+                rotate_helper(curr->parent,grandparent);
+            }
+            else if (parent == grandparent->right && curr == parent->right) { // right right case
+                parent->red = false;
+                grandparent->red = true;
+                grandparent = rotate_left(parent);
+                rotate_helper(curr->parent,grandparent);
+            }
+        }
+    }
+}
+
+void RedBlackTree::rotate_helper(Node* old_curr, Node* new_curr) { // reconnect the new root to the rest of the tree
+    if (old_curr->parent == nullptr) {
+        root = new_curr;
+    } else if (old_curr == old_curr->parent->left) {
+        old_curr->parent->left = new_curr;
+    } else {
+        old_curr->parent->right = new_curr;
+    }
+    if (new_curr) {
+        new_curr->parent = old_curr->parent;
+    }
+}
 
 // void RedBlackTree::insert(string name) {
 //     root = helperInsert(root, name);
@@ -68,6 +150,30 @@ void RedBlackTree::case_violation(Node* curr) {
 //     }
 // }
 
-void RedBlackTree::inOrder() {
-    inOrderHelper(this->root);
+void RedBlackTree::inOrder(Node* curr, bool& first) {
+    first = true;
+    if (curr == nullptr) {
+        return;
+    }
+    inOrder(curr->left,first);
+    if (!first) {
+        cout<<", ";
+    }
+    cout<<curr->rating;
+    first = false;
+    inOrder(curr->right,first);
+
 }
+
+Node* RedBlackTree::search(Node* curr, string name) {
+    if (curr == nullptr || curr->name == name) {
+        return curr;
+    }
+    if (name<curr->name){
+    return search(curr->left, name);
+    }
+    if (name>curr->name) {
+        return search(curr->right, name);
+    }
+}
+
--- DSA3-gamefinder/src/RedBlackTree.h
+++ DSA3-gamefinder/src/RedBlackTree.h
@@ -12,11 +12,11 @@ using namespace std;
 		Node* left;
 		Node* right;
 		Node* parent;
-		string _name;
+		string name;
 		int rating;
 		int height;
 		bool red; // each time we insert a node in a red black tree, it's red
-		Node(string x, int r, bool color = true, int h = 0) : _name(x), rating(r),height(h), red(color),left(nullptr), right(nullptr) {}
+		Node(string x, int r, bool color = true, int h = 0) : name(x), rating(r),height(h), red(color),left(nullptr), right(nullptr) {}
 
 	};
 // AMF 4/16/25 - RedBlackTree Class is based on https://www.onlinegdb.com/BygDgQCjI
@@ -26,10 +26,14 @@ public:
 	Node* root = nullptr;
 	// void node_color_fix(Node* node);
 	Node* helperInsert(Node* curr, string name, int rating);
-	void inOrderHelper(Node* root);
+	Node* rotate_left(Node* parent);
+	Node* search(Node* curr,string name);
+	Node* rotate_right(Node* parent);
+	void inOrder(Node* root, bool& first);
 	void case_violation(Node* curr);
+	void rotate_helper(Node* old_curr,Node* new_curr);
 	void insert(string name, int rating);
-	void inOrder();
+	// void inOrder();
 	void findTopTen();
 	void findTopTenGenre();
 	void findGame();
